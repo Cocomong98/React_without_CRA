472c37368136e344a848299ac37338b0d5cf554b
Cocomong98 <yonghjjm0618@gmail.com>
2025-03-03 23:46:40 +0900

    4강 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const DotenvWebpack = require("dotenv-webpack");
+
+const buildMode = process.env.NODE_ENV === "dev" ? "development" : "production";
+
 module.exports = {
-  entty: 
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.[hash].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    mode: "development",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html",
+        }),
+        new DotenvWebpack({
+            path: `./.env.${process.env.NODE_ENV || "dev"}`,
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 9000,
+        open: true,
+    },
 };
